1d02d0d4fa0154ba3e6cebe5f50e1d045859051f
dsrl1996 <david.rodriguez@codesa.com.co>
2019-08-15 11:36:18 -0500

    Agregamos y modificamos

---
+++ src/app/pages/pages.routes.ts
@@ -0,0 +1,19 @@
+import { Routes, RouterModule } from '@angular/router';
+
+import { ProgressComponent } from './progress/progress.component';
+import { Graficas1Component } from './graficas1/graficas1.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { PagesComponent } from './pages.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: PagesComponent,
+    children: [
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'progress', component: ProgressComponent },
+      { path: 'graficas1', component: Graficas1Component }
+    ]
+  },
+];
+export const PagesRouteModule = RouterModule.forChild(routes);
